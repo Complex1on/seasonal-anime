0784d913068fb2d5898c8cfa124fb6098eb7cc51
Evan Yip <etyip@ucsb.edu>
2019-09-18 13:06:53 -0700

    boiler plate code

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Header';
+
+const App = () => {
+    return (
+        <div className="container">
+            <Header />
+            <i class="ni ni-html5"></i>
+        </div>
+    );
+};
+
+export default App;
